075fece595860626af43ce2e420bd6c08daabd61
Caleb <chisom.ogbonnaya@yahoo.com>
2015-09-26 11:34:39 -0700

    added loads by id

--- php/user.class.php
+++ php/user.class.php
@@ -13,4 +13,30 @@
  */
 class User extends Database{
     private $username;
+    private $id;
+    private $isActivated;
+    
+    public function __get($name){
+        return $this->$name;
+    }
+    
+    public function __set($name, $value) {
+        $this->$name=$value;
+    }
+    
+    /**
+     * loads by id
+     * please test before using
+     * @param type $id
+     */
+    public function load_by_id($id){
+        $select = "SELECT * from user ";
+        $where = "WHERE id = $id limit 1;";
+        $result = mysqli_query($this->connect, $select . $where);
+        $row = mysqli_fetch_array($result);
+        foreach($row as $key => $value){
+            //populate properties witth value
+            echo "$key : $value";
+        }
+    }
 }
