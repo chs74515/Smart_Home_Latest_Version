ca2167a7066c3832281fe315e95fd63186f7c33b
Brody <brody.bruns695@topper.wku.edu>
2015-09-29 20:05:10 -0500

    added load by and save

--- php/appliance.class.php
+++ php/appliance.class.php
@@ -12,9 +12,61 @@
  * @author Brody
  */
 class Appliance extends Database{
-    protected $id;
-    protected $house_id;
+    protected $applianceId;
+    protected $houseId;
     protected $status;
     protected $type;
     
+    
+    public function __construct($id = null) {
+        parent::__construct();
+        if($id){
+            $this->load_by_id($id);
+        }
+    }
+    
+    public function __get($name){
+        return $this->$name;
+    }
+    
+    public function __set($name, $value) {
+        $this->$name=$value;
+    }
+    
+    public function load_by_id($id){
+        $select = "SELECT * from appliances ";
+        $where = "WHERE applianceId = $id limit 1;";
+        $result = mysqli_query($this->connect, $select . $where);
+        $row = mysqli_fetch_array($result);
+        if($result){
+            $row = mysqli_fetch_assoc($result);
+            foreach($row as $key => $value){
+                //populate properties witth value
+                echo "<br> [$key] => " . $this->$key;
+                $this->$key = $value;
+            }
+        }
+    }
+    
+    public function save(){
+        if($this->applianceId){
+            $this->update();
+        }else{
+            $this->insert();
+        }
+    }
+    
+    private function update(){
+        
+    }
+    
+    private function insert(){
+        $sql = "INSERT into `appliances` (`houseID`, `status`, `type`) "
+            . "VALUES ($this->houseId, $this->status, $this->type); ";
+        $result = mysqli_query($this->connect, $sql);
+        $row = mysqli_fetch_array($result);
+        foreach($row as $key=>$value){
+            echo "<br>$key => [$value]";
+        }
+    }
 }
