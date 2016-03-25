11fcc96950cee5f03ee163ddd0ecf88c002052ef
Thomas Murray <thomas.murray662@topper.wku.edu>
2016-03-25 09:42:37 -0500

    Began to work on the Schedule class.

---
+++ php/schedule.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of schedule
+ *
+ * @author Thomas Murray
+ */
+class Schedule {
+    private $id;
+    private $name;
+    private $lightGroup_id;
+    private $weekSchedule;
+    private $priority;
+    
+    public static function addSchedule($nameP, $lightGroup_id) {
+        
+    }   
+}
